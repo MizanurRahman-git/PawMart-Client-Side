dd7fbbe6503905a6b10dc82f45995e63822923b3
MizanurRahmanHridoy <mizanurrahmanhridoy42@gmail.com>
2026-01-04 03:41:16 +0600

    add pagination

--- src/Pages/PetsSupplies.jsx
+++ src/Pages/PetsSupplies.jsx
@@ -2,17 +2,29 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import LatestProducts from "../Components/LatestProducts";
+import { GrFormNextLink, GrFormPreviousLink } from "react-icons/gr";
 
 const PetsSupplies = () => {
   const [searchProducts, setSearchProducts] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [currentPages, setCurrentPages] = useState(0);
+  const [sort, setSort] = useState("date")
+  const [order, setOrder] = useState("");
+  const limit = 8;
 
   useEffect(() => {
-    fetch("https://pawmart-store-server.vercel.app/allproducts")
+    fetch(
+      `https://pawmart-store-server.vercel.app/allproducts?limit=${limit}&skip=${
+        currentPages * limit
+      }&sort=${sort}&order=${order}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setSearchProducts(data);
+        setSearchProducts(data?.result);
+        const page = Math.ceil(data?.total / limit);
+        setTotalPages(page);
       });
-  }, []);
+  }, [currentPages, order, sort]);
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -35,11 +47,21 @@ const PetsSupplies = () => {
   };
 
   const handleAllProducts = () => {
-    fetch("https://pawmart-store-server.vercel.app/allproducts")
+    fetch(
+      `https://pawmart-store-server.vercel.app/allproducts?limit=${limit}&skip=${
+        currentPages * limit
+      }`
+    )
       .then((res) => res.json())
       .then((data) => {
-        setSearchProducts(data);
+        setSearchProducts(data?.result);
       });
+  };
+
+  const handleSelect = (e) => {
+    const sortText = e.target.value
+    setSort(sortText.split("-")[0])
+    setOrder(sortText.split("-")[1]);
   };
 
   return (
@@ -50,7 +72,7 @@ const PetsSupplies = () => {
           <Navbar />
         </header>
         <main className="w-11/12 mx-auto">
-          <div className="mt-7 flex justify-between items-center">
+          <div className="mt-7 flex flex-col md:flex-row justify-between items-center">
             <div className="space-x-1">
               <select defaultValue="Select Category" className="select w-40">
                 <option>
@@ -95,8 +117,19 @@ const PetsSupplies = () => {
                 </option>
               </select>
             </div>
-            <div>
-              <form onSubmit={handleSearch} className="text-end mt-5">
+            <div className="flex flex-col md:flex-row items-center gap-9">
+              <div className="flex items-center mt-5 md:mt-0 gap-1.5">
+                <p>Sort_By:</p>
+                <select onChange={handleSelect} className="select">
+                  <option selected disabled={true}>
+                    Customized
+                  </option>
+                  <option value={"date-desc"}>Latest-Items</option>
+                  <option value={"price-asc"}>Low To High "Price"</option>
+                  <option value={"price-desc"}>High To Low "Price"</option>
+                </select>
+              </div>
+              <form onSubmit={handleSearch} className="text-end">
                 <div className="join">
                   <div>
                     <label className="input validator join-item">
@@ -120,9 +153,40 @@ const PetsSupplies = () => {
               <LatestProducts key={product._id} product={product} />
             ))}
           </div>
+
+          <div className=" flex justify-self-end gap-1.5 py-2 flex-wrap">
+            {currentPages > 0 && (
+              <button
+                onClick={() => setCurrentPages(currentPages - 1)}
+                className="btn flex items-center"
+              >
+                <GrFormPreviousLink />
+                Prev
+              </button>
+            )}
+
+            {[...Array(totalPages).keys()].map((i) => (
+              <button
+                onClick={() => setCurrentPages(i)}
+                className={`btn ${currentPages === i && "bg-black text-white"}`}
+              >
+                {i + 1}
+              </button>
+            ))}
+
+            {currentPages < totalPages - 1 && (
+              <button
+                onClick={() => setCurrentPages(currentPages + 1)}
+                className="btn flex items-center"
+              >
+                Next
+                <GrFormNextLink />
+              </button>
+            )}
+          </div>
         </main>
       </div>
-      <footer>
+      <footer className="mt-20">
         <Footer />
       </footer>
     </div>
